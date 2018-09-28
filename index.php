8abf54a435ae120b941a8cb5bc0dac7b903c2333
Juan Aguillon <miloca.98@hotmail.com>
2018-09-28 17:40:27 -0500

    crear index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<div class="all_content">
+
+<?php 
+
+  if ( have_posts( ) ){
+    if( is_home() && ! is_front_page() ){
+      while ( have_posts( ) ){
+        the_post( );
+      }
+    }
+  }else{
+    echo "No hay posts";
+  }
+
+?>
+
+</div>
