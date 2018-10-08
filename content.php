fcf95695c75e19ff9e26c7a5e0ce301f5e2779dc
Juan Aguillon <miloca.98@hotmail.com>
2018-10-08 08:03:00 -0500

    Permalink thumbnail

--- content.php
+++ content.php
@@ -3,7 +3,13 @@
 		<?php 
 
 		if ( has_post_thumbnail() ){
-			the_post_thumbnail();
+			?>
+				<div class="publication">
+					<a href="<?php the_permalink( ); ?>" title="<?php the_title_attribute(); ?>">
+						<?php the_post_thumbnail(); ?>
+					</a>
+				</div>	
+			<?php
 		}else{
 			echo '<h1>Np hay imagen</h1>';
 		}
