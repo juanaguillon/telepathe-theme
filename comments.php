50f54a7a25aa81d50bfbae2e4cf74f305f174cb0
Juan Aguillon <miloca.98@hotmail.com>
2018-10-12 16:24:44 -0500

    Editando Plantilla Comentarios

--- comments.php
+++ comments.php
@@ -1,1 +1,25 @@
-<h1>Plantilla COmentarios</h1>
+<?php 
+
+if ( post_password_required() ){
+	return;
+}
+
+ ?>
+
+<div id="comments_template">
+	<?php if ( have_comments() ){
+		?> 
+		<h3>
+			<?php 
+			printf( _n(
+				'Mostrando %s comentario ',
+				'Mostrando %s comentarios',
+				get_comments_number(  ),
+				"telepathe"
+			), get_comments_number() );
+
+			 ?>
+		</h3>
+		<?php
+	} ?>
+</div>
