461c87e4cafc0a3f267d42aac55c7c592993b968
Juan Aguillon <miloca.98@hotmail.com>
2018-10-19 17:24:55 -0500

    Agregando Scripts y Styles

--- functions.php
+++ functions.php
@@ -16,6 +16,15 @@ function telepathe_supports_theme( ){
 
 }
 
+function telepathe_linked_scripts(){
+	wp_register_style( 'styles', get_template_directory_uri() . '/styles.css' , '','1.0', 'all'  );
+	wp_enqueue_style( 'styles' );
+	wp_register_script( 'script', get_template_directory_uri() . '/script.js' , '', '1.0', false );
+	wp_enqueue_script( 'script' );
+}
+
+add_action('admin_enqueue_scripts', 'telepathe_linked_scripts');
+
 add_action('after_setup_theme','telepathe_supports_theme');
 add_action('widgets_init','telepathe_register_sidebar');
 
---
+++ script.js
@@ -0,0 +1,1 @@
+alert('Hola, estamos en el script');
