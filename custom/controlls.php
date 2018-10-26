d92c1cdc1f7d735eb5cc5302ac9ee57d8d9db99f
Juan Aguillon <miloca.98@hotmail.com>
2018-10-26 17:39:43 -0500

    Añadiendo un Control

---
+++ custom/controlls.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+function telepathe_register_controlls( $wp_customize ){
+
+  $wp_customize->add_control(
+    new WP_Customize_Color_Control( $wp_customize, 'telepathe_colors', array(
+      "label" => "Color Header/Footer",
+      "description" => "Selecciona el color de el Header y el Footer",
+      "section" => 'colors'
+    ) )
+  );
+  
+}
+
+add_action('customize_register','telepathe_register_controlls');
